cd09b0a3024cd1e673e34894d325e0dd61313cc6
ebernevec <eduard.bernevec8@gmail.com>
2021-02-17 17:13:20 +0200

    Implemented logs for Hooks

--- src/test/java/com/endava/soa_5/cucumber/hook/Hooks.java
+++ src/test/java/com/endava/soa_5/cucumber/hook/Hooks.java
@@ -1,19 +1,29 @@
 package com.endava.soa_5.cucumber.hook;
 
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.Status;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.lang.String.format;
 
 public class Hooks {
+    Logger log = LoggerFactory.getLogger(Hooks.class);
 
-    @Before
-    public void beforeScenario() {
-        //log.info("-----------------START OF SCENARIO----------------");
-        //TODO: uncomment when log is implemented
+    @BeforeEach
+    public void beforeScenario(Scenario scenario) {
+        String scenarioName = scenario.getName();
+        log.info("---------------------START OF SCENARIO--------------------");
+        log.info("*********************" + scenarioName + "*********************");
     }
 
     @After
-    public void afterScenario() {
-        //log.info("-----------------END OF SCENARIO----------------");
-        //TODO: uncomment when log is implemented
+    public void afterScenario(Scenario scenario) {
+        Status scenarioStatus = scenario.getStatus();
+        log.info("---------------------END OF SCENARIO--------------------");
+        log.info(format("ENDING TEST: [%s] %s ", scenario.getStatus().toString().toUpperCase(), scenario.getName()));
+//        log.info("*********************" + scenarioStatus + "*********************");
     }
 }
